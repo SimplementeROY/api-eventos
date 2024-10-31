53ae28540c450d976f53afb3757f7553a289d8a9
SimplementeROY <sherwinagunias11@gmail.com>
2024-10-31 10:11:14 +0100

    Arreglando el .env

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -34,7 +34,7 @@ exports.checkToken = async (req, res, next) => {
 }
 
 exports.checkAdmin = (req, res, next) => {
-    if (req.user.role !== 'admin') {
+    if (req.user.rol !== 'admin') {
         return res.status(403).json({ message: 'Solo puedes hacer esta acción si eres admin' });
     }
     next();
